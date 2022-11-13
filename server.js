fc3cf89895d8f42566e749728870b30633d6a7a5
netanel tesfay <netaneltes66@gmailgit config --global user.email you@example.com.com>
2022-11-13 13:20:24 +0200

    commit

---
+++ server.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const app = express();
+const cors = require("cors");
+const port = 8000;
+app.use(cors())
+app.use(express.json({ extended: true }));
+app.use(express.urlencoded({ extended: true }));
+
+app.get("/",(req,res)=>{
+    res.send("seccusse")
+})
+
+
+app.listen(port,()=>{
+    console.log(`server is up ${port}`);
+})
